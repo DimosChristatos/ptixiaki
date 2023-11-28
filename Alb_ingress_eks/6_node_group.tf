35dee68fb4b341338952e3eee413e8cef05be628
Dimos Christatos <christatos.dim@gmail.com>
2023-11-28 19:52:30 +0200

    added role dependancy

--- Alb_ingress_eks/6_node_group.tf
+++ Alb_ingress_eks/6_node_group.tf
@@ -15,13 +15,27 @@ resource "aws_iam_role" "my_eks_node_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "nodes_amazon_eks_worker_node_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.my_eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_amazon_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.my_eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_amazon_ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.my_eks_node_role.name
+}
 resource "aws_eks_node_group" "my_nodes" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "knot-nodes"
   node_role_arn   = aws_iam_role.my_eks_node_role.arn
   subnet_ids      = [
     aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_1.id
+    aws_subnet.public_subnet_2.id
   ]
   capacity_type = "SPOT"
   instance_types = ["t3a.small"]
@@ -31,4 +45,10 @@ resource "aws_eks_node_group" "my_nodes" {
     max_size     = 4
     min_size     = 1
   }
+
+  depends_on = [ 
+    aws_iam_role_policy_attachment.nodes_amazon_ec2_container_registry_read_only,
+    aws_iam_role_policy_attachment.nodes_amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.nodes_amazon_eks_worker_node_policy
+   ]
 }
